d74d94bddc2db33db2a5431ad0088766c4c312c4
L1ZAK <66622256+jayed5@users.noreply.github.com>
2026-05-23 12:34:00 +0200

    Refactor CleanStart.bat for improved functionality

--- CleanStart.bat
+++ CleanStart.bat
@@ -1,13 +1,16 @@
 @echo off
-net session >nul 2>&1 || (
-echo Run as Administrator!
-pause>error
-del error
-exit /b
+cd /d "%~dp0"
+
+fsutil dirty query %systemdrive% >nul 2>&1 || (
+    echo Run as Administrator!
+    pause>nul
+    exit /b
 )
+
 if not exist "C:\CleanLogs" (
-mkdir "C:\CleanLogs"
+    mkdir "C:\CleanLogs"
 )
+
 (
 echo "@echo off"
 echo "echo. >> C:\CleanLogs\clean_log_%%date%%.txt"
@@ -17,7 +20,7 @@ echo "echo ------------------------------------------------------- >> C:\CleanLo
 echo "echo. >> C:\CleanLogs\clean_log_%%date%%.txt"
 echo "del /q /f /s "%%TEMP%%\*" >> C:\CleanLogs\clean_log_%%date%%.txt 2>&1"
 echo "echo. >> C:\CleanLogs\clean_log_%%date%%.txt"
-echo "del /q /f "%%LocalAppData%%\Microsoft\Windows\Explorer\thumbcache_*.db" >> C:\CleanLogs\clean_log_%%date%%.txt 2>&1"
+echo "del /q /f "%%LocalAppData%%\Microslop\Windows\Explorer\thumbcache_*.db" >> C:\CleanLogs\clean_log_%%date%%.txt 2>&1"
 echo "ipconfig /flushdns >> C:\CleanLogs\clean_log_%%date%%.txt 2>&1"
 echo "echo. >> C:\CleanLogs\clean_log_%%date%%.txt"
 echo "forfiles /p "C:\CleanLogs" /s /m *.* /d -3 /c "cmd /c del /q @path" >> C:\CleanLogs\clean_log_%%date%%.txt 2>&1"
@@ -27,13 +30,14 @@ echo "echo End: %%date%% %%time%% >> C:\CleanLogs\clean_log_%%date%%.txt"
 echo "echo ------------------------------------------------------- >> C:\CleanLogs\clean_log_%%date%%.txt"
 echo "echo. >> C:\CleanLogs\clean_log_%%date%%.txt"
 echo "exit"
-) > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\CleanStart.bat"
-cd "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+) > "%APPDATA%\Microslop\Windows\Start Menu\Programs\Startup\CleanStart.bat"
+
+cd "%APPDATA%\Microslop\Windows\Start Menu\Programs\Startup"
 powershell -NoProfile -Command " $lines = [System.IO.File]::ReadAllLines('CleanStart.bat'); $lines | ForEach-Object { $_.TrimStart('\"').TrimEnd('\"') } | Set-Content 'CleanStart.bat' -Encoding ASCII "
+
 echo.
 echo -------------------------
 echo  You can close this now. 
 echo -------------------------
 echo.
-pause>e
-del e
+pause>nul
